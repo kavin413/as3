89b8460fbae38486f809e944e815cb647006e8ec
kavin413 <srisudhakavin@gmail.com>
2025-07-01 17:40:20 +0530

    Add files via upload

--- pyhton.py
+++ pyhton.py
@@ -1,9 +1,10 @@
 valid= False
 while not valid:
     try:
-        n=int(input("enter a number of your choice:"))
-        n%2==0
-        print("bye")
-    except:
+        n=int(input("enter a number of your choice: "))
+        while n%2==0:
+          print("bye")
+          valid= True
+    except ValueError:
         print("the number is not even ")
         
